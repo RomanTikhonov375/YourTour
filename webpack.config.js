1df7ff18ef974940c2f9b16524c455b11014ec7f
Roman Tikhonov <Roman.Tikhonov375@gmail.com>
2023-12-09 23:05:19 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][hash].js',
+    publicPath: ''
+  },
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node-modules/'
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+            filename: 'images/[name].[hash][ext]'
+        }
+    },
+    {
+        test: /\.(woff|woff2|eot|ttf)$/,
+        type: 'asset/resource',
+        generator: {
+            filename: 'fonts/[name].[hash][ext]'
+        }
+    }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'template.html'),
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+}; 
