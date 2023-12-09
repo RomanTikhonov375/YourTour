73eb5b57ae75781dfa707636e76a3ec512d4adc6
Roman Tikhonov <Roman.Tikhonov375@gmail.com>
2023-12-09 23:05:19 +0300

    feat add variables for scss and images

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
 import './index.scss';
-
-function component(text) {
-    const element = document.createElement('h1');
-    element.textContent = text;
-    return element;
-  }
-  
-  document.body.prepend(component('Проект собран на Webpack'));
